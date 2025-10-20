7f3de52621cb5e9cbaab9e1c637cc8a5dab961c4
taniacruzz <tanicruz112@gmail.com>
2025-10-20 00:41:11 -0300

    fix:etl-evolution-tickets-update

--- src/extract.py
+++ src/extract.py
@@ -1,39 +1,37 @@
 import pandas as pd
-from sqlalchemy import create_engine
-import urllib
 
 
-def extract_sqlserver_for_evolution_chart(config):
+def extract_sqlserver_for_evolution_chart():
     """
     Extrai dados do SQL Server usando SQLAlchemy para evitar warnings do pandas.
     Retorna o df da primeira data e df dos tickets (histórico completo).
     """
-    driver = config["driver"]
-    server = config["server"]
-    database = config["database"]
-    username = config["user"]
-    password = config["password"]
+    from nodesk.etl.databases import sqlserver
 
-    # String de conexão segura
-    params = urllib.parse.quote_plus(
-        f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
-    )
-    conn = create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
+     # Query para pegar a primeira data
+    query_first_date = """
+    SELECT TOP 1 Tickets.CreatedAt AS FirstCreatedAt
+    FROM Tickets
+    ORDER BY Tickets.CreatedAt ASC;
+    """
+    df_first_date = pd.read_sql(query_first_date, sqlserver)
 
-    query_first_date = "SELECT TOP 1 CreatedAt FROM Tickets ORDER BY createdat ASC;"
-    df_first_date = pd.read_sql(query_first_date, conn)
+    # Query para pegar histórico completo de tickets
+    query_tickets = """
+    SELECT Tickets.TicketId, FromStatusId, ToStatusId, ChangedAt,
+           Categories.Name AS Category, Subcategories.Name AS Subcategories, CreatedAt
+    FROM Tickets
+    LEFT JOIN TicketStatusHistory ON TicketStatusHistory.TicketId = Tickets.TicketId
+    JOIN Categories ON Tickets.CategoryId = Categories.CategoryId
+    JOIN Subcategories ON Tickets.SubcategoryId = Subcategories.SubcategoryId
+    """
+    df_tickets = pd.read_sql(query_tickets, sqlserver)
 
-    query_tickets = (
-        "SELECT TicketId, FromStatusId, ToStatusId, ChangedAt, "
-        "Categories.Name as Category, Sucategories.Name as Subcategories, CreatedAt "
-        "FROM TicketStatusHistory JOIN Ticket ON TicketStatusHistory.TicketId = Ticket.TicketId "
-        "JOIN Categories ON Ticket.CategoryId = Categories.CategoryId "
-        "JOIN Subcategories ON Ticket.SubcategoryId = Subcategory.SubcategoryId"
-    )
-    df_tickets = pd.read_sql(query_tickets)
+    # Debug rápido
+    print("df_tickets (preview):")
+    print(df_tickets.head(3))
 
     return df_first_date, df_tickets
-
 
 # def extract_postgres(config):
 #     conn = psycopg2.connect(
